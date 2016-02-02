3631a59f60f39e56fbdea7cb6ac2bc4a23ed2777
David Saxon <davidesaxon@gmail.com>
2016-02-03 01:48:49 +1300

    Linked against ChaosCore

--- src/alpha/Main.cpp
+++ src/alpha/Main.cpp
@@ -1,12 +1,21 @@
+// TODO: REMOVE ME
+#include <iostream>
+
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QLabel>
 
+#include <chaoscore/base/uni/UTF8String.hpp>
+
 #include "alpha/internal/qt/Window.hpp"
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
+    chaos::uni::UTF8String pls("!!");
+
+    std::cout << pls << std::endl;
+
     // TODO: move and use fake args
-    QApplication app( argc, argv );
+    QApplication app(argc, argv);
 
     // QLabel* window = new QLabel( "Hello World" );
     Window* window = new Window();
